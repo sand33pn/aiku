20db25dee2f83cfad450e5f29ef67eea8e47df0b
sand33pn <grayghost.2003@gmail.com>
2016-03-14 15:51:18 +0530

    Finish Chapter 7: Sign up

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,4 +11,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 		assert_template 'new'
 	end
 
+	test "valid signup information" do
+		get signup_path
+		assert_difference 'User.count', 1 do
+			post_via_redirect users_path, user: {name: "foobar", email: "user@valid.com",
+															             password: "foobar",
+															             password_confirmation: "foobar"}
+		end
+		assert_template 'users/show'
+	end
+
 end
