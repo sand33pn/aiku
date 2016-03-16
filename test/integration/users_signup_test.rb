fa55262a444506627570128121599bf85128522b
sand33pn <grayghost.2003@gmail.com>
2016-03-16 11:35:50 +0530

    Complete Section 8.2: Logging in

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -19,6 +19,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 															             password_confirmation: "foobar"}
 		end
 		assert_template 'users/show'
+		assert is_logged_in?
 	end
 
 end
