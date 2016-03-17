a4fb2159c3701694d2a1781cd18906bd17fbb8d8
sand33pn <grayghost.2003@gmail.com>
2016-03-17 12:56:41 +0530

    Complete Section 9.3: Showing all users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
 	#Returns the Gravatar for the given user
-	def gravatar_for(user)
+	def gravatar_for(user, options = { size: 80 })
 		image_tag("https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}",
 			        alt: user.name, class: "gravatar")
 	end
